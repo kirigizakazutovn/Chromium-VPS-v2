c59e29ecb83c32a8437842a6a286f07b365044de
Nguyễn Huy Hiếu <83147867+kirigizakazutovn@users.noreply.github.com>
2022-01-24 15:13:06 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+os.system("pip install selenium==3.141.0")
+from webbot import Browser
+web = Browser()
+web.go_to('https://m.youtube.com/watch?v=9EwqXZtviTM')
+website = input('Service has audio')
